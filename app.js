287ece68cbc489c917a6b11000ef55d9215c1078
Eli <30394324+Elijah-J@users.noreply.github.com>
2023-01-07 12:09:51 -0800

    allow user to add new tasks to list

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+/* document elements */
+const addTaskButton = document.getElementById("add-task");
+
+/* event listeners */
+addTaskButton.addEventListener("click", addTask);
+
+function addTask() {
+  const task = createTask();
+  const todoContainer = document.getElementById("todo-container");
+  todoContainer.appendChild(task);
+}
+
+function createTask() {
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+
+  checkbox.type = "checkbox";
+  const addTaskInput = document.getElementById("add-task-input");
+
+  const taskText = document.createTextNode(addTaskInput.value);
+  const removeTaskButton = document.createElement("button");
+  removeTaskButton.innerText = "Remove";
+
+  const fragment = document.createDocumentFragment();
+  task = fragment.appendChild(li).appendChild(checkbox);
+  li.appendChild(taskText);
+  li.appendChild(removeTaskButton);
+
+  return fragment;
+}
