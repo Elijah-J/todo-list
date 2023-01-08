66a5582527be22934fba2948a843f9be20bb73d1
Eli <30394324+Elijah-J@users.noreply.github.com>
2023-01-08 15:16:01 -0800

    refactor removing tasks implementing event delegation

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const addTaskContainer = document.querySelector("#add-task-container");
 addTaskContainer.addEventListener("submit", handleAddTask);
 
+const taskContainer = document.querySelector("#task-container");
+taskContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("display__button--remove")) {
+    handleRemoveTask(e);
+  }
+});
+
 const addTaskInput = document.querySelector("#add-task-input");
 const emptyStateMessage = document.querySelector("#empty-state-message");
 
@@ -9,7 +16,7 @@ function handleAddTask(event) {
   if (isValid(addTaskInput.value.trim())) {
     addTask();
   } else {
-    generateErrorMessage();
+    showErrorMessage();
   }
 
   if (
@@ -41,10 +48,10 @@ function createTask() {
   checkbox.type = "checkbox";
 
   const taskText = document.createTextNode(addTaskInput.value.trim());
+
   const removeTaskButton = document.createElement("button");
   removeTaskButton.innerText = "Remove";
   removeTaskButton.classList.add("display__button--remove");
-  removeTaskButton.addEventListener("click", handleRemoveTask);
 
   li.appendChild(checkbox);
   li.appendChild(taskText);
@@ -67,26 +74,19 @@ function getTaskCount() {
   return document.querySelector("#task-container").childElementCount;
 }
 
-function generateErrorMessage() {
-  const errorMessage = document.createElement("p");
-  errorMessage.innerText =
-    "Invalid input. Text must be between 1 and 100 characters long.";
-  errorMessage.classList.add("text--error");
-
-  const addTaskContainer = document.querySelector("#add-task-container");
-  addTaskContainer.appendChild(errorMessage);
+function showErrorMessage() {
+  const errorMessage = document.querySelector("#error-message");
+  errorMessage.classList.remove("text--hidden");
 
   const messageDuration = 2000;
   setTimeout(() => {
-    errorMessage.remove();
+    errorMessage.classList.add("text--hidden");
   }, messageDuration);
 }
 
 function removeEmptyStateMessage() {
   emptyStateMessage.classList.add("text--hidden");
 }
-
-const taskContainer = document.querySelector("#task-container");
 
 function generateUuid() {
   return "id" + Math.random().toString(16).slice(2);
